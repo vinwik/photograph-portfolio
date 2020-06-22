1875f3d12af812ae9b4f351e4bf109d028f52a9f
Vincent Wikart <vinwik@hotmail.com>
2020-06-22 23:34:47 +0100

    smooth scroll pollyfill

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import smoothscroll from "smoothscroll-polyfill";
+
+// kick off the polyfill!
+smoothscroll.polyfill();
 
 ReactDOM.render(
   <React.StrictMode>
